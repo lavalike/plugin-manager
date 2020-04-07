5aff82c132c9cc843588d373955376ecbdcba769
王震 <lavalike@yeah.net>
2020-04-07 10:14:56 +0800

    plugin version control

--- app/src/main/java/com/wangzhen/plugin/host/MainActivity.java
+++ app/src/main/java/com/wangzhen/plugin/host/MainActivity.java
@@ -1,9 +1,12 @@
 package com.wangzhen.plugin.host;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,7 +21,17 @@ import com.wangzhen.plugin.PluginManager;
 import com.wangzhen.plugin.callback.PluginLoadCallback;
 import com.wangzhen.plugin.helper.FileUtils;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * MainActivity
@@ -57,19 +70,72 @@ public class MainActivity extends AppCompatActivity {
             if (PackageManager.PERMISSION_GRANTED != checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
                 requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE);
             } else {
-                downloadAndLoad();
+                checkPluginVersion();
             }
         } else {
-            downloadAndLoad();
+            checkPluginVersion();
         }
     }
 
-    private void downloadAndLoad() {
-        String url = "http://192.168.188.199:8080/wangzhen/plugin/apk/app-release.apk";
+    private void checkPluginVersion() {
+        Request request = new Request.Builder()
+                .url("http://192.168.188.199:8080/wangzhen/plugin/plugin.json")
+                .build();
+        new OkHttpClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, final IOException e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        mTvMsg.append("an error occur, load cache version\n");
+                        downloadAndLoad(getPreferences().getString("url", ""));
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (response.code() == 200 && response.body() != null) {
+                            try {
+                                JSONObject jsonObject = new JSONObject(response.body().string());
+                                int version_code = jsonObject.getInt("version_code");
+                                String url = jsonObject.getString("url");
+                                if (version_code > getPreferences().getInt("version_code", 0)) {
+                                    mTvMsg.append("new version found\n");
+                                    //更新版本
+                                    getPreferences().edit().putInt("version_code", version_code).putString("url", url).apply();
+                                    downloadAndLoad(url);
+                                } else {
+                                    //加载旧版本
+                                    mTvMsg.append("no new version found, load cache version\n");
+                                    downloadAndLoad(getPreferences().getString("url", ""));
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            mTvMsg.append("an error occur, load cache version\n");
+                            downloadAndLoad(getPreferences().getString("url", ""));
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    private void downloadAndLoad(String url) {
+        if (TextUtils.isEmpty(url)) {
+            return;
+        }
         String path = FileUtils.getInternalPluginPath(this);
         File file;
         if ((file = new File(path, FileUtils.getFileName(url))).exists()) {
-            mTvMsg.append("plugin exists, load from local cache\n");
+            mTvMsg.append("plugin exists, load cache plugin\n");
             loadPlugin(file.getAbsolutePath());
             return;
         }
@@ -94,6 +160,10 @@ public class MainActivity extends AppCompatActivity {
                 .build());
     }
 
+    private SharedPreferences getPreferences() {
+        return getSharedPreferences("plugin", Context.MODE_PRIVATE);
+    }
+
     private void loadPlugin(String path) {
         PluginManager.getInstance().load(path, new PluginLoadCallback() {
             @Override
@@ -114,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (REQUEST_CODE == requestCode) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                downloadAndLoad();
+                checkPluginVersion();
             } else {
                 Toast.makeText(this, "请开启存储权限", Toast.LENGTH_SHORT).show();
             }
